67151c402007f84540dc2275d330592c79703f22
Shujaat Hussain <shujaat.apple05@gmail.com>
2025-03-20 02:11:08 +0400

    Catalog Microservice ->  Adding the database implementation to the create product feature.

--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,10 @@
-using System.Security.Cryptography;
-using BuildingBlocks.CQRS;
-using Catalog.Api.Models;
-using MediatR;
-
 namespace Catalog.Api.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -22,9 +17,11 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
             Price = command.Price
         };
         
-        //TODO: Save entity to the database
+        //Save entity to the database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
         
         //Return the result
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }

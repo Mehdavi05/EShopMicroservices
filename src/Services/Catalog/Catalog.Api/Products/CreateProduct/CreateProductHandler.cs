7e22daec5cda52ae79a535893256ba19fa248ff5
Shujaat Hussain <shujaat.apple05@gmail.com>
2025-03-19 22:22:44 +0400

    Catalog Microservice -> Adding Command and Query abstractions to the BuildingBlocks project

--- src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 namespace Catalog.Api.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price): IRequest<CreateProductResult>;
+
 public record CreateProductResult(Guid Id);
 
 internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>

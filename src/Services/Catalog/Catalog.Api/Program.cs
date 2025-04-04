3e2ed138c4e846d2b26dd22a4c01935c8f65b1c5
Shujaat Hussain <shujaat.apple05@gmail.com>
2025-04-04 20:37:07 +0400

    Catalog Microservice -> Global handling exceptions in ASP.NET Core with app.UseExceptionHandler

--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -1,4 +1,7 @@
 using BuildingBlocks.Behaviors;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ErrorContext = Newtonsoft.Json.Serialization.ErrorContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +26,32 @@ var app = builder.Build();
 
 //Configure the http Request Pipeline.
 app.MapCarter();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if (exception == null)
+        {
+            return; 
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,  
+            Detail = exception.StackTrace
+        };
+        
+        var logger = context.RequestServices.GetService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+        
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+        
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();
